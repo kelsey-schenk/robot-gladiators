a48503cf24f7d320ce18b58ecfff6c7c438fd282
kelsey-schenk <kelsey.schenk@att.net>
2021-11-06 14:00:11 -0400

    multiple rounds coplete

--- assets/js/game.js
+++ assets/js/game.js
@@ -75,8 +75,17 @@ var fight = function(enemyName) {
     //     fight();
     //     window.alert("You need to choose a valid option. Try again!");
     // }
+
 for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+        // pick new enemy to fight based on the index of the enemyNames array
+        var pickedEnemyName = enemyNames[i];
+        // reset enemyHealth before starting new fight
+        enemyHealth = 50;
+        // use debugger to pause script from running and check what's going on
+        // debugger;
+        fight(pickedEnemyName);
+    }
 }
